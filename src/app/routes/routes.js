666f5c126f1b3d26b897a16f481a3e5bcb3e6a98
Alisson Matos <alissonmgsantos@gmail.com>
2019-09-06 09:03:17 -0300

    Conclusão do crud

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -30,8 +30,23 @@ module.exports = (app) => {
             .catch(erro => console.log(erro));
     });
 
-    app.get('/livros/form', function (req, resp) {
-        resp.marko(require('../views/livros/form/form.marko'))
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
+    });
+
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroService = new LivroService(db);
+    
+        livroService.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    
     });
 
     app.post('/livros', function(req, resp) {
@@ -41,11 +56,19 @@ module.exports = (app) => {
             .catch(erro => console.log(erro));
     });
 
+    app.put('/livros', function(req, resp) {
+        const livroService = new LivroService(db);
+        
+        livroService.atualiza(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
     app.delete('/livros/:id', function(req, resp) {
         const id = req.params.id;
     
-        const livroDao = new LivroDao(db);
-        livroDao.remove(id)
+        const livroService = new LivroService(db);
+        livroService.remove(id)
             .then(() => resp.status(200).end())
             .catch(erro => console.log(erro));
     
--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -5,12 +5,23 @@ require('marko/express');
 const express = require('express');
 const app = express();
 
+const methodOverride = require('method-override');
+
 const bodyParser = require('body-parser');
 
 app.use('/estatico', express.static('src/app/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+}));
 
 //Importando o modulo de rotas e repassando express para rota 
 const routes = require('../app/routes/routes');
